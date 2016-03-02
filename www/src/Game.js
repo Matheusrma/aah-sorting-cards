075ca81925eaf8bf74892ad62618226cc37d9698
matheusrma <matheusrma@matheusrma-macbookair.roam.corp.google.com>
2016-03-02 12:39:33 -0800

    Added Bucket logic

--- www/src/Game.js
+++ www/src/Game.js
@@ -1,17 +1,36 @@
+Bucket = function(x,y,w,h) {
+	this.rect = new Phaser.Rectangle(x,y,w,h);
+};
+
+Bucket.prototype = {
+	isInside: function(pos) {
+		var topLeft = this.rect.topLeft;
+		var bottomRight = this.rect.bottomRight;
+
+		return pos.x > topLeft.x && pos.y > topLeft.y && pos.x < bottomRight.x && pos.y < bottomRight.y;
+	}
+}
+
 SortCards.Game = function(game) {};
 
 SortCards.Game.prototype = {
 	preload: function(){
 		this.load.image('card', 'img/card.png');
 	},
-	create: function() {
-		var card = this.add.sprite(150, 223, 'card');
 
-    //  Enable input and allow for dragging
-    card.inputEnabled = true;
-    card.input.enableDrag();
-    card.events.onDragStart.add(onDragStart, this);
-    card.events.onDragStop.add(onDragStop, this);
+	create: function() {
+		for (var i = 0; i < 10; ++i){
+			var card = this.add.sprite(i * 100, 500, 'card');
+
+	    card.inputEnabled = true;
+	    card.input.enableDrag();
+	    card.events.onDragStart.add(this.onDragStart, this);
+	    card.events.onDragStop.add(this.onDragStop, this);
+		}
+
+		this.buckets = [
+			new Bucket(100,100,200,200)
+		]
 	},
 
 	onDragStart : function(sprite, pointer) {
@@ -19,6 +38,15 @@ SortCards.Game.prototype = {
 	},
 
 	onDragStop: function(sprite, pointer) {
-
+		if (this.checkForBucket(sprite.position) >= 0)
+			sprite.destroy()
 	},
+
+	checkForBucket : function(pos) {
+		for (var i = 0; i < this.buckets.length; ++i){
+			if (this.buckets[i].isInside(pos)) return i;
+		}
+
+		return -1;
+	}
 };
