93a53ee3e469f97d826ed5634472f0e4097162f5
matheusrma <matheusrma@matheusrma-macbookair.roam.corp.google.com>
2016-03-03 14:20:13 -0800

    File Refactor

--- www/src/Boot.js
+++ www/src/Boot.js
@@ -1,1 +1,15 @@
 var SortCards = {};
+
+SortCards.Boot = function(game) {};
+
+SortCards.Boot.prototype = {
+	preload: function() {
+	},
+	create: function() {
+		this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.game.scale.pageAlignHorizontally = true;
+		this.game.scale.pageAlignVertically = true;
+    
+		this.game.state.start('Preloader');
+	}
+};
