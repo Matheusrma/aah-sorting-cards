5443178a48157fab73259df7fe7552607cc1467b
jasmeet0817 <jasmeetsingh@jasmeetsingh-macbookair.roam.corp.google.com>
2016-10-31 11:11:52 +0100

    remove files added by mistake

--- end.js
+++
@@ -1,9 +0,0 @@
-SortCards.End = function(game) {};
-
-SortCards.End.prototype = {
-  create: function() {
-    this.background = this.add.tileSprite(0, 0, 1280, 750, 'background');
-    window.open('img/end_video.mp4', '_blank');
-    this.game.state.start('MainMenu');
-  },
-};
