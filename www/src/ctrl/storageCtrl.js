d8672a71687fe97bcf35dbe2b97f6042440a86ea
jasmeet0817 <jasmeetsingh@jasmeetsingh-macbookair.roam.corp.google.com>
2016-10-31 12:13:13 +0100

    Rollforward commit a345aaeb3a5c0d733b2fdcf7777deb8d076b458c

--- www/src/ctrl/storageCtrl.js
+++ www/src/ctrl/storageCtrl.js
@@ -23,7 +23,7 @@ StorageCtrl = function(){}
 
 StorageCtrl.LOCAL_STORAGE_KEY_ = 'template_results';
 
-StorageCtrl.USER_ID = -1;
+StorageCtrl.SESSION_ID = -1;
 
 StorageCtrl.prototype = {
 
@@ -32,7 +32,7 @@ StorageCtrl.prototype = {
   },
 
   generateNewUserId: function(){
-    StorageCtrl.USER_ID = this.generateRandomId()
+    StorageCtrl.SESSION_ID = this.generateRandomId()
   },
 
   saveTemplateResult:function(templateId, buckets){
@@ -41,12 +41,18 @@ StorageCtrl.prototype = {
     if (template_results.length > 0)
       template_results = JSON.parse(JSON.parse(template_results));
 
+    for (var i =  template_results.length - 1; i >= 0; --i){
+       if (template_results[i].templateId == templateId && 
+           template_results[i].session_id == StorageCtrl.SESSION_ID){
+         template_results.splice(i,1)
+       }
+     }
+
     new_template_result = {};
     new_template_result.id = this.generateRandomId();
+    new_template_result.timestamp = new Date();
     new_template_result.templateId = templateId;
-    new_template_result.user = {
-      id: StorageCtrl.USER_ID
-    }
+    new_template_result.session_id = StorageCtrl.SESSION_ID
 
     new_template_result.cardsMapping = [];
 
