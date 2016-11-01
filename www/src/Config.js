92788dfee93581e7b02576045efde19341a6dfa9
danngr <dan.greenblatt+github@gmail.com>
2016-11-01 22:42:07 +0700

    Changed config file to accomodate new set cards
    These cards are specific to Preah Vihear

--- www/src/Config.js
+++ www/src/Config.js
@@ -20,9 +20,9 @@ Config = {
     ],
     CARDS: {
         // Type: [COUNT, IMAGE_PREFIX, IMAGE/PATH_PREFIX, CARD_POSITION]
-        VEGETARIAN: [16, 'card_vegetable_', 'img/vegetable_', 0],
-        MEAT: [4, 'card_meat_', 'img/meat_', 1],
-        LEGUME: [5, 'card_legume_', 'img/legume_', 2],
+        VEGETARIAN: [10, 'card_vegetable_', 'img/vegetable_', 0],
+        MEAT: [0, 'card_meat_', 'img/meat_', 1],
+        LEGUME: [0, 'card_legume_', 'img/legume_', 2],
         FRUIT: [10, 'card_fruit_', 'img/fruit_', 3],
     STARCH: [6, 'card_starch_', 'img/starch_', 4]
     }
