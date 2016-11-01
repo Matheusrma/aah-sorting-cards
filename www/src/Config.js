02162c3139291894019fdfdae4ca837572ec6e98
jasmeet0817 <jasmeetsingh@jasmeetsingh-macbookair.roam.corp.google.com>
2016-11-01 16:39:51 +0100

    Bring back config translations

--- www/src/Config.js
+++ www/src/Config.js
@@ -4,19 +4,19 @@
 Config = {
     TEMPLATES:
     [
-        new TitleBar('Which foods are you most likely to feed your youngest child?', ProgressBar.CARD_SET_TYPE.FOOD),
-        new TitleBar('Sort the foods by how expensive they are.', ProgressBar.CARD_SET_TYPE.FOOD),
-		new TitleBar('Which foods are easiest to get year-round?', ProgressBar.CARD_SET_TYPE.FOOD),
-        new TitleBar('Which foods are easily available to buy on the market?', ProgressBar.CARD_SET_TYPE.FOOD),
-		new TitleBar('How often do you feed these foods to your child?', ProgressBar.CARD_SET_TYPE.FOOD),
+        new TitleBar('តើអាហារមួយណាដែលអ្នកនឹងផ្តល់ឲ្យកូនតូចជាងគេរបស់អ្នក?', ProgressBar.CARD_SET_TYPE.FOOD),
+        new TitleBar('ចូររៀបអាហារទាំងនេះទៅតាមតម្លៃរបស់វា.', ProgressBar.CARD_SET_TYPE.FOOD),
+    new TitleBar('តើអាហារមួយណាដែលងាយស្រួលរកបានជាងគេ ពេញមួយឆ្នាំ?', ProgressBar.CARD_SET_TYPE.FOOD),
+        new TitleBar('តើអាហារមួយណាដែលងាយស្រួលទិញពីផ្សារ?', ProgressBar.CARD_SET_TYPE.FOOD),
+    new TitleBar('តើអ្នកផ្តល់អាហារទាំងនេះដល់កូនញឹកញាប់យ៉ាងណាដែរ?', ProgressBar.CARD_SET_TYPE.FOOD),
         /* new TitleBar('The End', ProgressBar.CARD_SET_TYPE.END), */
     ],
     BUCKETS: [
-      ['Would feed', 'Would not feed'],
-      ['Very cheap', 'Cheap', 'Expensive', 'Very expensive'],
-      ['Not easy', 'Easy'],
-      ['Not easy', 'Easy'],
-      ['Never', 'Once a week', 'Several days a week', 'Everyday'],
+      ['នឹងផ្តល់', 'នឹងមិនផ្តល់'],
+      ['ថោកជាងគេ', 'ថោក', 'ថ្លៃ', 'ថ្លៃជាងគេ'],
+      ['មិនងាយរកបាន', 'ងាយរកបាន'],
+      ['មិនងាយទិញ', 'ងាយទិញ'],
+      ['មិនដែលសោះ', 'ម្តងក្នុងមួយសប្តាហ៍', 'ច្រើនដងក្នុងមួយសប្តាហ៍', 'រៀងរាល់ថ្ងៃ'],
     ],
     CARDS: {
         // Type: [COUNT, IMAGE_PREFIX, IMAGE/PATH_PREFIX, CARD_POSITION]
@@ -24,6 +24,6 @@ Config = {
         MEAT: [4, 'card_meat_', 'img/meat_', 1],
         LEGUME: [5, 'card_legume_', 'img/legume_', 2],
         FRUIT: [10, 'card_fruit_', 'img/fruit_', 3],
-		    STARCH: [6, 'card_starch_', 'img/starch_', 4]
+    STARCH: [6, 'card_starch_', 'img/starch_', 4]
     }
 }
